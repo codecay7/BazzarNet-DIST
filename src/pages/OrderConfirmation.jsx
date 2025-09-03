acc01b875474d4b5419a09f58a3a78fc1d037e64
codeCULT-01 <codecult01@gmail.com>
2025-09-04 05:18:37 +0530

    [dyad] Fixed qrcode.react import error by using default import - wrote 1 file(s)

--- src/pages/OrderConfirmation.jsx
+++ src/pages/OrderConfirmation.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { AppContext } from '../context/AppContext';
-import { QRCode } from 'qrcode.react'; // Corrected import: named export
+import QRCode from 'qrcode.react'; // Corrected import: default export
 
 const OrderConfirmation = () => {
   const { user } = useContext(AppContext);
