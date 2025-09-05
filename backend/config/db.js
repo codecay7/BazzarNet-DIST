33bb60decb21fcbb3b14a43a5c9592cb67824932
codeCULT-01 <codecult01@gmail.com>
2025-09-05 10:46:46 +0530

    [dyad] Clarified that the provided MongoClient code is not used, as the project uses Mongoose for MongoDB connections. - wrote 1 file(s)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import env from './env.js'; // Import environment variables
+import env from './config/env.js'; // Import environment variables
 
 const connectDB = async () => {
   try {
