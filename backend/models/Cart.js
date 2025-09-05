2e4f9e921f73787bbae8d51454f2f8a47de853a5
codeCULT-01 <codecult01@gmail.com>
2025-09-05 20:24:15 +0530

    [dyad] Fixed a syntax error in the Cart model definition. - wrote 1 file(s)

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const cartItemSchema = new new mongoose.Schema({
+const cartItemSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Product',
