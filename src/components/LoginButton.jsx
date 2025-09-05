3e3e7425284a56aeee65e972312ffb276c0fc81f
codeCULT-01 <codecult01@gmail.com>
2025-09-06 00:31:02 +0530

    [dyad] Fixed the LoginButton's text and icon color to be white for better visibility. - wrote 1 file(s)

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -15,7 +15,7 @@ const LoginButton = () => {
         <div className="user-profile-inner">
           <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <g data-name="Layer 2" id="Layer_2">
-              <path d="m15.626 11.769a6 6 0 1 0 -7.252 0 9.008 9.008 0 0 0 -5.374 8.231 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 9.008 9.008 0 0 0 -5.374-8.231zm-7.626-4.769a4 4 0 1 1 4 4 4 4 0 0 1 -4-4zm10 14h-12a1 1 0 0 1 -1-1 7 7 0 0 1 14 0 1 1 0 0 1 -1 1z" fill="var(--text)" />
+              <path d="m15.626 11.769a6 6 0 1 0 -7.252 0 9.008 9.008 0 0 0 -5.374 8.231 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 9.008 9.008 0 0 0 -5.374-8.231zm-7.626-4.769a4 4 0 1 1 4 4 4 4 0 0 1 -4-4zm10 14h-12a1 1 0 0 1 -1-1 7 7 0 0 1 14 0 1 1 0 0 1 -1 1z" fill="#fff" /> {/* Changed fill to #fff */}
             </g>
           </svg>
           <p>Log In</p>
@@ -54,19 +54,19 @@ const StyledWrapper = styled.div`
     width: 127px;
     height: 47px;
     border-radius: 13px;
-    background-color: #1a1a1a; /* Keeping this dark gray as per design */
+    background-color: #1a1a1a;
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 15px;
-    color: var(--text); /* Using theme text color */
+    color: #fff; /* Changed color to #fff */
     font-weight: 600;
   }
 
   .user-profile-inner svg {
     width: 27px;
     height: 27px;
-    fill: var(--text); /* Using theme text color for SVG fill */
+    fill: #fff; /* Changed fill to #fff */
   }
 `;
 
